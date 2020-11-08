de4614a90f7c6a36e3f9c454caae7c0434dfc02c
Mathias Korsbäck <mathias@korsback.org>
2020-11-08 21:45:37 +0100

    Initial commit.

---
+++ DailyNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNews
+//
+//  Created by Mathias Korsbäck on 2020-11-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
